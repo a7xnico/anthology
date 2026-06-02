3aa30b85707c696b17c5b29d6481532e04e75cea
matiasMCI <matiascoli2017@gmail.com>
2026-06-02 10:41:11 -0300

    Union de Controllers -AdminArtistController y ArtistController-

--- src/main/java/com/anthology/controller/ArtistController.java
+++ src/main/java/com/anthology/controller/ArtistController.java
@@ -1,6 +1,7 @@
 package com.anthology.controller;
 
 
+import com.anthology.dto.requests.ArtistRequest;
 import com.anthology.dto.responses.ArtistResponse;
 import com.anthology.service.ArtistService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,15 +11,58 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/artists")
 @AllArgsConstructor
-@Tag(name = "Artistas", description = "Consulta de Artistas")
+@Tag(name = "Admin - Artistas", description = "Gestion y consulta de Artistas")
 public class ArtistController {
     private final ArtistService artistService;
+
+
+    ///  privados
+
+
+    @Operation(summary = "Crear Artista", description = "Crea un nuevo Artista en el sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Artista Creado correctamente"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada invalidos"),
+            @ApiResponse(responseCode = "409", description = "Ya existe un Artista con ese Stage Name")
+    })
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ArtistResponse> createArtist(@Valid @RequestBody ArtistRequest artistRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(artistService.createArtist(artistRequest));
+    }
+
+
+    @Operation(summary = "Modificar Artista", description = "Modifica los datos de un Artista")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Artista actualizado correctamente"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada invalido"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
+    })
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ArtistResponse> updateArtist(@Parameter(description = "Id del Artista") @PathVariable Long id, @Valid @RequestBody ArtistRequest artistRequest){
+        return ResponseEntity.status(HttpStatus.OK).body(artistService.updateById(id,artistRequest));
+    }
+
+
+
+    /// publicos
+    @Operation(summary = "Buscar Artistas", description = "Buscar todos los Artistas")
+    @ApiResponse(responseCode = "200", description = "Busqueda realizada correctamente")
+    @GetMapping
+    public ResponseEntity<List<ArtistResponse>> findAllArtist(){
+        return ResponseEntity.ok(artistService.findAllArtist());
+    }
 
     @Operation(summary = "Buscar Artista especifico", description = "Devuelve datos de un Artista especifico buscado por ID")
     @ApiResponses({
