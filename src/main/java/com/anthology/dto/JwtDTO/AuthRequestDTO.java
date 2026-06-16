fb54f1cfee7be45951577b0dcefc024cb62d5a40
matiasMCI <matiascoli2017@gmail.com>
2026-06-16 19:35:12 -0300

    se agrego documentacion para Swagger

--- src/main/java/com/anthology/dto/JwtDTO/AuthRequestDTO.java
+++ src/main/java/com/anthology/dto/JwtDTO/AuthRequestDTO.java
@@ -1,10 +1,22 @@
 package com.anthology.dto.JwtDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "DTO para autenticar un usuario en el sistema")
 public record AuthRequestDTO(
-        @NotBlank String username,
-        @NotBlank String password
+        @Schema(
+                description = "Nombre de usuario",
+                example = "Juan"
+        )
+        @NotBlank(message = "El username no puede estar vacío")
+        String username,
+        @Schema(
+                description = "Contraseña del usuario",
+                example = "Juansito555"
+        )
+        @NotBlank(message = "La contraseña no puede estar vacía")
+        String password
 ) {
 
 }
